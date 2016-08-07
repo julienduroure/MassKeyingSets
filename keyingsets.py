4a2a54791249f3fee8be5593670f3653fb5279d6
Julien Duroure <julien.duroure@gmail.com>
2016-08-07 21:06:37 +0200

    take into account objects, not only bones

--- keyingsets.py
+++ keyingsets.py
@@ -5,7 +5,7 @@ bl_info = {
 	"blender": (2, 77, 0),
 	"location": "Scene properties",
 	"warning": "Alpha version",
-	"description": "Create KeyingSet from bone selection",
+	"description": "Create KeyingSet from selection",
 	#"wiki_url": "",
 	#"tracker_url": ""
 	"category": "Animation"}
@@ -54,6 +54,8 @@ class POSE_OT_juks_keying_from_selected(bpy.types.Operator):
 	rotation = bpy.props.BoolProperty(default=True)
 	scale    = bpy.props.BoolProperty(default=False)
 	bone_custom_props = bpy.props.BoolProperty(name="Bone Custom Properties", default=False)
+	obj_custom_props  = bpy.props.BoolProperty(name="Object Custom Properties", default=False)
+	data_custom_props  = bpy.props.BoolProperty(name="Object Data Custom Properties", default=False)
 	only_non_locked = bpy.props.BoolProperty(name="Do not key locked channels", default=True)
 
 	@classmethod
@@ -73,7 +75,12 @@ class POSE_OT_juks_keying_from_selected(bpy.types.Operator):
 		row.prop(self, "rotation")
 		row.prop(self, "scale")
 		row = layout.row()
-		row.prop(self, "bone_custom_props")
+		if check_case(context)[1] == "BONES":
+			row.prop(self, "bone_custom_props")
+		else:
+			row.prop(self, "obj_custom_props")
+			row = layout.row()
+			row.prop(self, "data_custom_props")
 		row = layout.row()
 		row.prop(self, "only_non_locked")
 
@@ -141,6 +148,78 @@ class POSE_OT_juks_keying_from_selected(bpy.types.Operator):
 					for prop in bone.keys():
 						if prop != "_RNA_UI":
 							ksp = ks.paths.add(bone.id_data, bone.path_from_id() + "[\""+ prop  + "\"]", index=-1)
+
+		elif check_case(context)[1] == "OBJ":
+			scene = context.scene
+			ks = scene.keying_sets.new(idname="KeyingSet", name=self.name)
+			ks.bl_description = ""
+
+			armature_found = False
+			for obj in context.selected_objects:
+				if obj.type != "ARMATURE":
+					if self.location == True:
+						if self.only_non_locked == True:
+							if obj.lock_location[0] == False and obj.lock_location[1] == False and obj.lock_location[2] == False:
+								ksp = ks.paths.add(obj, 'location', index=-1)
+							else:
+								if obj.lock_location[0] == False:
+									ksp = ks.paths.add(obj, 'location', index=0)
+								if obj.lock_location[1] == False:
+									ksp = ks.paths.add(obj, 'location', index=1)
+								if obj.lock_location[2] == False:
+									ksp = ks.paths.add(obj, 'location', index=2)
+						else:
+							ksp = ks.paths.add(obj, 'location', index=-1)
+
+					if self.rotation == True:
+						if obj.rotation_mode == "QUATERNION":
+							if obj.lock_rotation_w == False and obj.lock_rotation[0] == False and obj.lock_rotation[1] == False and obj.lock_rotation[2] == False:
+								ksp = ks.paths.add(obj, 'rotation_quaternion', index=-1)
+							else:
+								if obj.lock_rotation_w == False:
+									ksp = ks.paths.add(obj, 'rotation_quaternion', index=0)
+								if obj.lock_rotation[0] == False:
+									ksp = ks.paths.add(obj, 'rotation_quaternion', index=1)
+								if obj.lock_rotation[1] == False:
+									ksp = ks.paths.add(obj, 'rotation_quaternion', index=2)
+								if obj.lock_rotation[2] == False:
+									ksp = ks.paths.add(obj, 'rotation_quaternion', index=3)
+						elif obj.rotation_mode == "AXIS_ANGLE":
+							if obj.lock_rotation_w == False and obj.lock_rotation[0] == False and obj.lock_rotation[1] == False and obj.lock_rotation[2] == False:
+								ksp = ks.paths.add(obj, 'rotation_angle', index=-1)
+							else:
+								if obj.lock_rotation_w == False:
+									ksp = ks.paths.add(obj, 'rotation_angle', index=0)
+								if obj.lock_rotation[0] == False:
+									ksp = ks.paths.add(obj, 'rotation_angle', index=1)
+								if obj.lock_rotation[1] == False:
+									ksp = ks.paths.add(obj, 'rotation_angle', index=2)
+								if obj.lock_rotation[2] == False:
+									ksp = ks.paths.add(obj, 'rotation_angle', index=3)
+						else:
+							if obj.lock_rotation[0] == False and obj.lock_rotation[1] == False and obj.lock_rotation[2] == False:
+								ksp = ks.paths.add(obj, 'rotation_euler', index=-1)
+							else:
+								if obj.lock_rotation[0] == False:
+									ksp = ks.paths.add(obj, 'rotation_euler', index=0)
+								if obj.lock_rotation[1] == False:
+									ksp = ks.paths.add(obj, 'rotation_euler', index=1)
+								if obj.lock_rotation[2] == False:
+									ksp = ks.paths.add(obj, 'rotation_euler', index=2)
+					if self.scale == True:
+						ksp = ks.paths.add(obj, 'scale', index=-1)
+					if self.obj_custom_props == True:
+						for prop in obj.keys():
+							if prop != "_RNA_UI":
+								ksp = ks.paths.add(obj, "[\""+ prop  + "\"]", index=-1)
+					if self.data_custom_props == True:
+						for prop in obj.data.keys():
+							if prop != "_RNA_UI":
+								ksp = ks.paths.add(obj.data, "[\""+ prop  + "\"]", index=-1)
+				else:
+					armature_found = True
+			if armature_found == True:
+				self.report({'WARNING'}, "Armatures not taken into account")
 
 		return {'FINISHED'}
 
