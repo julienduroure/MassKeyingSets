017210be967b1da38b131a493bbf4fdd9c4d2ea8
Julien Duroure <julien.duroure@gmail.com>
2016-08-09 18:22:27 +0200

    Add license

--- masskeyingsets.py
+++ masskeyingsets.py
@@ -1,3 +1,26 @@
+##########################################################################################
+#	GPL LICENSE:
+#-------------------------
+# This file is part of MassKeyingSets.
+#
+#    MassKeyingSets is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    MassKeyingSets is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with MassKeyingSets.  If not, see <http://www.gnu.org/licenses/>.
+##########################################################################################
+#
+#	Copyright 2016 Julien Duroure (contact@julienduroure.com)
+#
+##########################################################################################
+
 bl_info = {
 	"name": "Create KeyingSet from Selection",
 	"author": "Julien Duroure",
